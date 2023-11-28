ffe274645a798f20233afce185b8ab405761fc88
Oguz Kanda <exporler95@gmail.com>
2023-11-28 14:22:06 +0300

    Initial Commit

---
+++ InsatgarmTutorial/InsatgarmTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsatgarmTutorialApp.swift
+//  InsatgarmTutorial
+//
+//  Created by Oğuz Kanda on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InsatgarmTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
